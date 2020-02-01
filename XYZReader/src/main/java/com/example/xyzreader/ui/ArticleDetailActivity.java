4dc225946b07c3fab5bd2ca7ad57966dff69658d
ZHANG Yaohua <zhangyaohua40605@gmail.com>
2020-02-01 12:54:59 +0800

    the appbar layout in detail page is done

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -9,16 +9,18 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.SubtitleCollapsingToolbarLayout;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
+import android.widget.ImageView;
 
 import com.example.xyzreader.R;
 import com.example.xyzreader.data.ArticleLoader;
@@ -42,6 +44,9 @@ public class ArticleDetailActivity extends AppCompatActivity
 //    private View mUpButtonContainer;
 //    private View mUpButton;
 
+    public SubtitleCollapsingToolbarLayout toolbarLayout;
+    public ImageView imageInAppbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,14 +56,18 @@ public class ArticleDetailActivity extends AppCompatActivity
                             View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         }
         setContentView(R.layout.activity_article_detail);
-        Toolbar myChildToolbar = (Toolbar) findViewById(R.id.my_child_toolbar);
+//        Toolbar myChildToolbar = (Toolbar) findViewById(R.id.my_child_toolbar);
 //        myChildToolbar.setTitle("detail page");
-        setSupportActionBar(myChildToolbar);
+//        setSupportActionBar(myChildToolbar);
         // Get a support ActionBar corresponding to this toolbar
-        ActionBar ab = getSupportActionBar();
-        ab.setTitle("Detail Page");
+//        ActionBar ab = getSupportActionBar();
+//        ab.setTitle("Detail Page");
         // Enable the Up button
-        ab.setDisplayHomeAsUpEnabled(true);
+//        ab.setDisplayHomeAsUpEnabled(true);
+
+
+//        imageInAppbar = findViewById(R.id.image_in_appbar);
+//        toolbarLayout = findViewById(R.id.subtitlecollapsingtoolbarlayout);
 
         getLoaderManager().initLoader(0, null, this);
 //add new line
@@ -184,7 +193,7 @@ public class ArticleDetailActivity extends AppCompatActivity
             super.setPrimaryItem(container, position, object);
             ArticleDetailFragment fragment = (ArticleDetailFragment) object;
             if (fragment != null) {
-                mSelectedItemUpButtonFloor = fragment.getUpButtonFloor();
+//                mSelectedItemUpButtonFloor = fragment.getUpButtonFloor();
 //                updateUpButtonPosition();
             }
         }
