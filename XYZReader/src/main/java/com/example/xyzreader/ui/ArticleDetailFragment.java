caab7a39df6752cee3854a87e6c083457893a31e
ZHANG Yaohua <zhangyaohua40605@gmail.com>
2020-02-01 12:54:59 +0800

    the appbar layout in detail page is done

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -1,9 +1,9 @@
 package com.example.xyzreader.ui;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
+import android.app.Fragment;
+import android.app.LoaderManager;
 import android.content.Intent;
-import android.support.v4.content.Loader;
+import android.content.Loader;
 import android.database.Cursor;
 
 
@@ -28,7 +28,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 
 import com.android.volley.VolleyError;
@@ -60,14 +59,13 @@ public class ArticleDetailFragment extends Fragment implements
     // Use default locale format
     private SimpleDateFormat outputFormat = new SimpleDateFormat();
     // Most time functions can only handle 1902 - 2037
-    private GregorianCalendar START_OF_EPOCH = new GregorianCalendar(2, 1, 1);
+    private GregorianCalendar START_OF_EPOCH = new GregorianCalendar(2,1,1);
 
     private SubtitleCollapsingToolbarLayout toolbarLayout;
     private ImageView imageView;
     private AdapterRecyclerView adapter;
     private Toolbar toolbar;
     private ActionBar ab;
-
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
@@ -110,7 +108,7 @@ public class ArticleDetailFragment extends Fragment implements
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+            Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_article_detail, container, false);
 
 
@@ -125,22 +123,8 @@ public class ArticleDetailFragment extends Fragment implements
         toolbarLayout = mRootView.findViewById(R.id.subtitlecollapsingtoolbarlayout);
         toolbar = mRootView.findViewById(R.id.my_child_toolbar);
         ((ArticleDetailActivity) getActivity()).setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                                                 @Override
-                                                 public void onClick(View v) {
-//                                                     Toast.makeText(getContext(),"your icon was clicked", Toast.LENGTH_SHORT).show();
-                                                     ((ArticleDetailActivity) getActivity()).onBackPressed();
-                                                 }
-                                             }
-        );
-
-
         ab = ((ArticleDetailActivity) getActivity()).getSupportActionBar();
-
-//        ab.setDisplayHomeAsUpEnabled(true);
-//        ab.setDisplayShowHomeEnabled(true);
-
+        ab.setDisplayHomeAsUpEnabled(true);
 
         mRootView.findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -197,7 +181,7 @@ public class ArticleDetailFragment extends Fragment implements
 
             } else {
 
-                toolbarLayout.setSubtitle(Html.fromHtml(
+               toolbarLayout.setSubtitle(Html.fromHtml(
                         outputFormat.format(publishedDate) + " by <font color='#ffffff'>"
                                 + mCursor.getString(ArticleLoader.Query.AUTHOR)
                                 + "</font>"));
